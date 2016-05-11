8696459b84d2f0808f77c76cd032cc27c532afc8
JessicaHerford <jrherford12@gmail.com>
2016-05-11 16:10:50 -0600

    added test module

---
+++ tests/functional/index.js
@@ -0,0 +1,12 @@
+define(function (require) {
+    var registerSuite = require('intern!object');
+    var assert = require('intern/chai!assert');
+
+    registerSuite({
+        name: 'index',
+
+        'greeting form': function () {
+
+        }
+    });
+});
